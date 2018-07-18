bd9761964d4db1b59008ccc0bb8dc60baad0ac41
zaher88abd <zaher88abd@gmail.com>
2018-07-18 16:04:37 -0300

    get test

--- dataset.py
+++ dataset.py
@@ -1,13 +1,19 @@
-def get_data_set():
-    data = list()
-    label = list()
-    try:
-        with open('Data/train-dev/train.txt', 'r', encoding="utf8") as f:
-            lines = f.readlines()
-        for line in lines:
-            arr = line.split("\t")
-            data.append(arr[0])
-            label.append(arr[1])
-    except Exception as ex:
-        print(ex)
-    return data, label
+class DataSet:
+    def get_test_data_set(file_name='Data/test/test.txt'):
+        with open(file_name, 'r', encoding="utf8")as f:
+            data = f.readlines()
+        return data
+
+    def get_train_data_set(file_name='Data/train-dev/train.txt'):
+        data = list()
+        label = list()
+        try:
+            with open(file_name, 'r', encoding="utf8") as f:
+                lines = f.readlines()
+            for line in lines:
+                arr = line.split("\t")
+                data.append(arr[0])
+                label.append(arr[1])
+        except Exception as ex:
+            print(ex)
+        return data, label
