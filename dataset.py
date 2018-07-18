9f18accca0a87f5db86f41b439fc80e9c3f554e5
zaher88abd <zaher88abd@gmail.com>
2018-07-18 16:20:28 -0300

    make get data class

--- dataset.py
+++ dataset.py
@@ -1,19 +1,29 @@
 class DataSet:
-    def get_test_data_set(file_name='Data/test/test.txt'):
-        with open(file_name, 'r', encoding="utf8")as f:
-            data = f.readlines()
-        return data
+    def __init__(self):
+        self.tranning.data = None
+        self.tranning.label = None
+        self.testing.data = None
 
-    def get_train_data_set(file_name='Data/train-dev/train.txt'):
-        data = list()
-        label = list()
-        try:
-            with open(file_name, 'r', encoding="utf8") as f:
-                lines = f.readlines()
-            for line in lines:
-                arr = line.split("\t")
-                data.append(arr[0])
-                label.append(arr[1])
-        except Exception as ex:
-            print(ex)
-        return data, label
+    def get_test_data_set(self, file_name='Data/test/test.txt'):
+        if self.testing.data is not None:
+            return self.testing.data
+        with open(file_name, 'r', encoding="utf8")as f:
+            self.testing.data = f.readlines()
+        return self.testing.data
+
+    def get_train_data_set(self, file_name='Data/train-dev/train.txt'):
+        if self.tranning.data is not None:
+            return self.tranning.data, self.tranning.label
+        else:
+            self.tranning.data = list()
+            self.tranning.label = list()
+            try:
+                with open(file_name, 'r', encoding="utf8") as f:
+                    lines = f.readlines()
+                for line in lines:
+                    arr = line.split("\t")
+                    self.tranning.data.append(arr[0])
+                    self.tranning.data.append(arr[1])
+            except Exception as ex:
+                print(ex)
+            return self.tranning.data, self.tranning.label
