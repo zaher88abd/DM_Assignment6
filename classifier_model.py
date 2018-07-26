0c26d12889f458fdeebb1925c67bcb2a27d585f3
zaher88abd <zaher88abd@gmail.com>
2018-07-26 18:46:23 -0300

    Show PCA chart

---
+++ classifier_model.py
@@ -0,0 +1,79 @@
+import matplotlib.pyplot as plt
+from sklearn.decomposition import TruncatedSVD
+from sklearn.feature_extraction.text import CountVectorizer
+from dataset import DataSet
+# from sklearn.feature_selection import chi2, SelectKBest
+import random
+
+data_set = DataSet()
+data, label = data_set.get_train_data_set()
+
+count_vect = CountVectorizer()
+X_train_counts = count_vect.fit_transform(data)
+
+truncatedSVD = TruncatedSVD(n_components=10, n_iter=7, random_state=42)
+truncatedSVD = truncatedSVD.fit(X_train_counts)
+
+X_r = truncatedSVD.transform(X_train_counts)
+colors = ['navy', 'turquoise', 'darkorange', 'navajowhite', 'salmon'
+    , 'azure', 'blue', 'brown', 'cadetblue', 'limegreen', 'maroon'
+    , 'cornsilk', 'peachpuff', 'red']
+
+languages = ('bg', 'mk', 'bs', 'hr', 'sr', 'cz', 'sk', 'es_AR', 'es_ES'
+             , 'pt-BR', 'pt-PT', 'id', 'my', 'xx')
+
+fig, ax = plt.subplots()
+
+c1 = ''
+c2 = ''
+c3 = ''
+c4 = ''
+c5 = ''
+c6 = ''
+c7 = ''
+c8 = ''
+c9 = ''
+c10 = ''
+c11 = ''
+c12 = ''
+c13 = ''
+c14 = ''
+comp1 = 0
+comp2 = 1
+# comp3 = 3
+for i in random.sample(range(len(X_r)), 5000):
+    if label[i] == 'bg':
+        c1 = ax.scatter(X_r[i, comp1], X_r[i, comp2], label='bg', color=colors[0])
+    if label[i] == 'mk':
+        c2 = ax.scatter(X_r[i, comp1], X_r[i, comp2], label='mk', color=colors[1])
+    if label[i] == 'bs':
+        c3 = ax.scatter(X_r[i, comp1], X_r[i, comp2], label='bs', color=colors[2])
+    if label[i] == 'hr':
+        c4 = ax.scatter(X_r[i, comp1], X_r[i, comp2], label='hr', color=colors[3])
+    if label[i] == 'sr':
+        c5 = ax.scatter(X_r[i, comp1], X_r[i, comp2], label='sr', color=colors[4])
+    if label[i] == 'cz':
+        c6 = ax.scatter(X_r[i, comp1], X_r[i, comp2], label='cz', color=colors[5])
+    if label[i] == 'sk':
+        c7 = ax.scatter(X_r[i, comp1], X_r[i, comp2], label='sk', color=colors[6])
+    if label[i] == 'es-AR':
+        c8 = ax.scatter(X_r[i, comp1], X_r[i, comp2], label='es_AR', color=colors[7])
+    if label[i] == 'es-ES':
+        c9 = ax.scatter(X_r[i, comp1], X_r[i, comp2], label='es_ES', color=colors[8])
+    if label[i] == 'pt-BR':
+        c10 = ax.scatter(X_r[i, comp1], X_r[i, comp2], label='pt-BR', color=colors[9])
+    if label[i] == 'pt-PT':
+        c11 = ax.scatter(X_r[i, comp1], X_r[i, comp2], label='pt-PT', color=colors[10])
+    if label[i] == 'id':
+        c12 = ax.scatter(X_r[i, comp1], X_r[i, comp2], label='id', color=colors[11])
+    if label[i] == 'my':
+        c13 = ax.scatter(X_r[i, comp1], X_r[i, comp2], label='my', color=colors[12])
+    if label[i] == 'xx':
+        c14 = ax.scatter(X_r[i, comp1], X_r[i, comp2], label='xx', color=colors[13])
+
+scatters = [c1, c2, c3, c4, c5, c6, c7, c8, c9, c10, c11, c12, c13, c14]
+
+ax.legend(handles=scatters)
+ax.grid(True)
+
+plt.show()
--- dataset.py
+++ dataset.py
@@ -1,29 +1,29 @@
 class DataSet:
     def __init__(self):
-        self.tranning.data = None
-        self.tranning.label = None
-        self.testing.data = None
+        self.tranning_data = None
+        self.tranning_label = None
+        self.testing_data = None
 
     def get_test_data_set(self, file_name='Data/test/test.txt'):
-        if self.testing.data is not None:
-            return self.testing.data
+        if self.testing_data is not None:
+            return self.testing_data
         with open(file_name, 'r', encoding="utf8")as f:
-            self.testing.data = f.readlines()
-        return self.testing.data
+            self.testing_data = f.readlines()
+        return self.testing_data
 
     def get_train_data_set(self, file_name='Data/train-dev/train.txt'):
-        if self.tranning.data is not None:
-            return self.tranning.data, self.tranning.label
+        if self.tranning_data is not None:
+            return self.tranning_data, self.tranning_label
         else:
-            self.tranning.data = list()
-            self.tranning.label = list()
+            self.tranning_data = list()
+            self.tranning_label = list()
             try:
                 with open(file_name, 'r', encoding="utf8") as f:
                     lines = f.readlines()
                 for line in lines:
                     arr = line.split("\t")
-                    self.tranning.data.append(arr[0])
-                    self.tranning.data.append(arr[1])
+                    self.tranning_data.append(arr[0])
+                    self.tranning_label.append(arr[1].replace("\n", ''))
             except Exception as ex:
                 print(ex)
-            return self.tranning.data, self.tranning.label
+            return self.tranning_data, self.tranning_label
