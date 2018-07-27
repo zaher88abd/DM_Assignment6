43dcfb9d62c0a7f763fc528370aedf06fb9f3c9e
zaher88abd <zaher88abd@gmail.com>
2018-07-26 21:50:36 -0300

    Add chi2 file

---
+++ ChiChart.py
@@ -0,0 +1,16 @@
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_selection import SelectKBest, chi2
+from dataset import DataSet
+from ShowChart import showChart
+
+
+def getChiChart(numComp=2):
+    data_set = DataSet()
+    data, label = data_set.get_train_data_set()
+
+    count_vect = CountVectorizer()
+    X_train_counts = count_vect.fit_transform(data)
+
+    X_new = SelectKBest(chi2, k=numComp).fit_transform(X_train_counts, label)
+
+    showChart(x=X_new, y=label, title="TruncatedSVD Language parameters ")
