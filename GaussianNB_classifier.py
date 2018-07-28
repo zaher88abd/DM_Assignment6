7d083228725b56cd210be1c33fcf32ea99352b6c
zaher88abd <zaher88abd@gmail.com>
2018-07-28 14:21:54 -0300

    Add combined feature

--- GaussianNB_classifier.py
+++ GaussianNB_classifier.py
@@ -1,6 +1,5 @@
 from sklearn.naive_bayes import GaussianNB
-from sklearn.model_selection import GridSearchCV
-
+from sklearn.metrics import confusion_matrix
 from mlxtend.preprocessing import DenseTransformer
 from sklearn.model_selection import train_test_split
 from sklearn.feature_extraction.text import CountVectorizer
@@ -8,10 +7,10 @@ from sklearn.feature_selection import SelectKBest, chi2
 from dataset import DataSet
 from sklearn.metrics import f1_score
 from sklearn.metrics import accuracy_score
-from sklearn.pipeline import Pipeline
-from MatrixPlot import plot_confusion_matrix
-from sklearn.metrics import confusion_matrix
+from sklearn.pipeline import Pipeline, FeatureUnion
 import matplotlib.pyplot as plt
+from MatrixPlot import plot_confusion_matrix
+from sklearn.decomposition import TruncatedSVD
 import random
 
 data_set = DataSet()
@@ -23,12 +22,14 @@ label = label[indexs]
 X_train, X_test, y_train, y_test = train_test_split(data, label, test_size=0.33, random_state=42)
 
 count_vect = CountVectorizer()
-chi2_model = SelectKBest(chi2, k=1000)
+selectKBest = SelectKBest(k=2000)
+truncatedSVD = TruncatedSVD(n_components=5, n_iter=7, random_state=42)
+combined_features = FeatureUnion([("chi2", chi2()), ("univ_select", selectKBest)])
 dense_transformer = DenseTransformer()
 clf_NB = GaussianNB()
 
 pipeline_NB = Pipeline([
-    ('count_v', CountVectorizer()), ('selectKB', chi2_model), ('to_dens', DenseTransformer()), ('clf', clf_NB)])
+    ('count_v', CountVectorizer()), ('features', combined_features), ('to_dens', DenseTransformer()), ('clf', clf_NB)])
 
 pipeline_NB = pipeline_NB.fit(X_train, y_train)
 y_pred = pipeline_NB.predict(X_test)
--- LinearSVC_classifier.py
+++ LinearSVC_classifier.py
@@ -7,26 +7,30 @@ from sklearn.feature_selection import SelectKBest, chi2
 from dataset import DataSet
 from sklearn.metrics import f1_score
 from sklearn.metrics import accuracy_score
-from sklearn.pipeline import Pipeline
+from sklearn.pipeline import Pipeline, FeatureUnion
 import matplotlib.pyplot as plt
 from MatrixPlot import plot_confusion_matrix
+from sklearn.decomposition import TruncatedSVD
 import random
 
 data_set = DataSet()
 data, label, class_names = data_set.get_train_data_set()
 
-indexs = random.sample(range(len(data)), 25000)
+indexs = random.sample(range(len(data)), 45000)
 data = data[indexs]
 label = label[indexs]
 X_train, X_test, y_train, y_test = train_test_split(data, label, test_size=0.25, random_state=42)
 
 count_vect = CountVectorizer()
-chi2_model = SelectKBest(chi2, k=2000)
+selectKBest = SelectKBest(k=2000)
+truncatedSVD = TruncatedSVD(n_components=5, n_iter=7, random_state=42)
+combined_features = FeatureUnion([("chi2", chi2()), ("univ_select", selectKBest)])
 dense_transformer = DenseTransformer()
 clf_SVC = LinearSVC(C=0.75, random_state=0, max_iter=500)
 
 pipeline_SVC = Pipeline(
-    [('count_v', CountVectorizer()), ('selectKB', chi2_model), ('to_dens', DenseTransformer()), ('clf', clf_SVC)])
+    [('count_v', CountVectorizer()), ('features', combined_features), ('to_dens', DenseTransformer()),
+     ('clf', clf_SVC)])
 
 pipeline_SVC = pipeline_SVC.fit(X_train, y_train)
 y_pred = pipeline_SVC.predict(X_test)
